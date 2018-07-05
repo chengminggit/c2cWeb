a98b36d95d7c0bd41208da0ee3708b436fe8e94e
zhyunlong <414228693@qq.com>
2018-07-05 19:58:25 +0800

    订单id自增

--- dboperate/formal_reservation_add.php
+++ dboperate/formal_reservation_add.php
@@ -6,14 +6,14 @@
      * Time: 20:36
      */
 
-    $tid = $_GET['tid'];
+/*    $tid = $_GET['tid'];
     $tidentify = $_GET['tidentify'];
     $lid = $_GET['lid'];
     $lidentify = $_GET['lidentify'];
     $hid = $_GET['hid'];
     $sdate = $_GET['sdate'];
     $rent = $_GET['rent'];
-    $number = $_GET['number'];
+    $number = $_GET['number'];*/
 
     //$servername = "localhost";
     //$username = "test";
@@ -29,12 +29,12 @@
         echo("连接成功");
     }
     $conn->query("set names utf8");
-  /*  $sql = "select max(id) from Reservation";
+    $sql = "select max(id) as id from Reservation";
     $result = $conn->query($sql);
     $raw = $result->fetch_assoc();
-    echo ("<tr><td>".$row["id"]."</td>");*/
-    $sql= "INSERT INTO Reservation  VALUES ('10005', '$hid', '$lid','$lidentify','$tid','$tidentify','1','$sdate','$rent','$number')";
-    //$sql= "INSERT INTO Reservation VALUES ('10010', '1001', '1005','5545115','10001','1121151','1','2018-10-10','500','5')";
+    $id = $raw["id"]+1;
+    echo "$id";
+    $sql= "INSERT INTO Reservation  VALUES ('$id', '$hid', '$lid','$lidentify','$tid','$tidentify','1','$sdate','$rent','$number')";
     $result = $conn->query($sql);
     $conn->close();
     echo json_encode("1");
