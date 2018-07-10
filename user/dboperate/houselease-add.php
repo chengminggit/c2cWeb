c4707b15caa2f57575d33110d76e9314f56d5bed
zzy <804379936@qq.com>
2018-07-10 11:50:38 +0800

    register

--- user/dboperate/houselease-add.php
+++ user/dboperate/houselease-add.php
@@ -5,13 +5,21 @@
  * Date: 2018/7/5
  * Time: 9:24
  */
-$name = $_GET['lessorName'];
-$phone = $_GET['lessorPhone'];
-$communityName = $_GET['county'];
+$lessorID = $_GET['lessorID'];
+$city = $_GET['city'];
+$county = $_GET['county'];
+$street = $_GET['street'];
+$area = $_GET['area'];
 $rent = $_GET['rent'];
-$time = $_GET['rentTime'];
-$houseType = $_GET['houseType'];
+$type = $_GET['type'];
 
+//$lessorID = 1001;
+//$city = '威海';
+//$county = '环翠区';
+//$street = '怡园街道';
+//$area = 100;
+//$rent = 1000;
+//$type = 3;
 /*if($_GET['communityName']==null ||$_GET['communityName']=="" ){
     $communityName = "字段为空";
 }*/
@@ -24,11 +32,15 @@ if ($conn->connect_error) {
 else{
     echo("连接成功");
 }
+$sql = "select max(ID) as id from house";
+$result = $conn->query($sql);
+$raw = $result->fetch_assoc();
+$id = $raw["id"]+1;
 $conn->query("set names utf8");
-$sql= "INSERT INTO houseInfo (lessorName, lessorPhone, cName, rent, rentTime, houseType) VALUES ('$name', '$phone', '$communityName', '$rent', '$time', '$houseType')";
+$sql= "INSERT INTO house (ID, lessorID, status, city, county, street, area, rent, discount,house.type) VALUES ('$id','$lessorID', 1,'$city', '$county', '$street', '$area', '$rent', 1, '$type')";
 
 $result = $conn->query($sql);
 $conn->close();
-echo json_encode("1");
+//echo json_encode("1");
 ?>
 
